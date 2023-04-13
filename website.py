3ef89f9f36a3e9e50f94ebae763b06328405a21c
Swagenwagen <tannedswegion@gmail.com>
2023-04-12 23:50:20 -0700

    session complete

--- website.py
+++ website.py
@@ -1,38 +1,25 @@
-from flask import Flask, render_template, request, jsonify, make_response
-# from flask_session.__init__ import Session
-# import secrets
-# import redis
+from flask import Flask, render_template, request, jsonify, session
 from interface import load_data, compareCareers, stats
-import datetime
+import secrets
+import redis
 
 app = Flask(__name__, template_folder="templates", static_folder="static")
 
-# r = redis.Redis()
-# r.mset({"Guesses": 5})
+app.secret_key = secrets.token_hex()
+
+r = redis.Redis()
 
 load_data()
 
 answer = "Kareem Abdul-Jabbar" # use redis for this
 
-# @app.before_first_request
-# def init():
-#     resp = make_response()
-#     resp.set_cookie('guesses', '5')
-#     return resp
-
 @app.route("/", methods=["POST", "GET"])
 def index():
-    resp = make_response(render_template('index.html'))
-    # if ('guesses' not in request.cookies):
-    if (True):
-        expire_date = datetime.datetime.now()
-        expire_date = expire_date + datetime.timedelta(days=1, minutes=0, seconds=1500)
-        # resp.set_cookie('expiry_date', f"{expire_date}", expires=expire_date)
-        resp.set_cookie('guesses', '5', expires=expire_date)
-    app.logger.info(request.cookies)
-        
-    return resp
-    # return render_template('index.html')
+    if (session.get("userID") == None):
+        session["userID"] = secrets.token_hex(4)
+        session.permanent = True
+        r.mset({session["userID"]: 5})
+    return render_template('index.html')
 
 @app.route("/submit_guess", methods=['POST'])
 def handleForm():
@@ -45,16 +32,13 @@ def retrieveCareers():
     careers = jsonify(load_data.careers)
     return careers
 
-# @app.route("/num_guesses", methods=['POST', 'GET'])
-# def numGuesses():
-#     # app.logger.info(r.get("Guesses"))
-#     # if request.method == 'POST':
-#     #     r.set("Guesses", int(r.get("Guesses")) - 1)
-#     # return jsonify({"num_guesses": int(r.get("Guesses"))})
-#     resp = make_response()
-#     if request.method == 'POST':
-#         resp.set_cookie('guesses', str(int(request.cookies.get('guesses')) - 1))
-#     return jsonify({"num_guesses": request.cookies.get('guesses')})
+@app.route("/num_guesses", methods=['POST', 'GET'])
+def numGuesses():
+    key = session["userID"]
+    if (request.method == 'POST'):
+        r.set(key, int(r.get(key))-1)
+    return jsonify({"num_guesses": int(r.get(key))})
+    
 
 @app.route("/retrieve_categories", methods=['GET'])
 def retrieveCategories():
@@ -62,5 +46,6 @@ def retrieveCategories():
 
 @app.route("/reset", methods=['GET'])
 def reset():
-    # return jsonify({"num_guesses": int(r.get("Guesses"))})
-    return jsonify({"num_guesses": request.cookies.get('guesses')})
+    key = session["userID"]
+    # r.set(key, 5)
+    return jsonify({"num_guesses": int(r.get(key))})
